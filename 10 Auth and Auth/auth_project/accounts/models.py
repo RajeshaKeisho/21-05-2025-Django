33c3f911eaec99f80dd19b4882ccc370a6ab2af7
Rajesh <rajarajesha@gmail.com>
2025-08-19 18:42:19 +0530

    New

---
+++ 10 Auth and Auth/auth_project/accounts/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+    is_verified = models.BooleanField(default=False)  # New field for email verification
+    email_verification_token = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return self.username
+
